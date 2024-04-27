52038c5c66945e146655e449897b246f5a731a85
user <user@account.com>
2024-04-27 08:06:35 +0200

    Initial Commit

---
+++ km-lab2-zad1-memory/km_lab2_zad1_memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  km_lab2_zad1_memoryApp.swift
+//  km-lab2-zad1-memory
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct km_lab2_zad1_memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
