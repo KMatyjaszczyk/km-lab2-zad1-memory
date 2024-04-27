490b6c5f5d1ef7517ba1bd0093994a8de2b347c5
user <user@account.com>
2024-04-27 09:37:59 +0200

    display cards in responsive grid, refactor

--- km-lab2-zad1-memory/ContentView.swift
+++ km-lab2-zad1-memory/ContentView.swift
@@ -12,18 +12,26 @@ struct ContentView: View {
     @State var cardsCount = 4
     var body: some View {
         VStack {
-            HStack {
-                ForEach(0 ..< cardsCount, id: \.self) { i in
-                    CardView(content: emojis[i])
-                }
-            }.padding()
-            HStack {
-                adjustCardNumber(by: -2, symbol: "minus.square")
-                Spacer()
-                adjustCardNumber(by: 2, symbol: "plus.square")
-            }.padding()
+            cardDisplay
+            cardNumberAdjuster
         }.padding()
         
+    }
+    
+    var cardDisplay: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0 ..< cardsCount, id: \.self) { i in
+                CardView(content: emojis[i])
+            }
+        }.foregroundColor(.blue)
+    }
+    
+    var cardNumberAdjuster: some View {
+        HStack {
+            adjustCardNumber(by: -2, symbol: "minus.square")
+            Spacer()
+            adjustCardNumber(by: 2, symbol: "plus.square")
+        }.padding()
     }
     
     func adjustCardNumber(by offset: Int, symbol: String) -> some View {
